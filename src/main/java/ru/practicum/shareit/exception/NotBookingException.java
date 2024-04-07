fa3aa5b80a8351aa374b1989ebd0a0e070f860e0
Pavel Pogorelskiy <akantor23@yandex.ru>
2024-04-07 18:03:03 +0300

    Корректировка NotBookingException

--- src/main/java/ru/practicum/shareit/exception/NotBookingException.java
+++ src/main/java/ru/practicum/shareit/exception/NotBookingException.java
@@ -1,9 +1,6 @@
 package ru.practicum.shareit.exception;
 
 public class NotBookingException extends RuntimeException {
-    public NotBookingException() {
-    }
-
     public NotBookingException(String message) {
         super(message);
     }
